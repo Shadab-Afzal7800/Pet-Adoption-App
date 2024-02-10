30cc4b40fe8717a7097a09fd9cf5be078cbb8bf5
Shadab-Afzal7800 <shadabafzal7800@gmail.com>
2024-02-10 12:44:12 +0530

    formatted code

--- lib/body.dart
+++ lib/body.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pet_shop_app/banner.dart';
-import 'package:pet_shop_app/constants.dart';
+
 import 'package:pet_shop_app/model/petsDetails.dart';
+
+import 'availableListContainers.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -45,54 +47,6 @@ class _BodyState extends State<Body> {
           // AdoptionListContainer(),
         ],
       ),
-    );
-  }
-}
-
-class AdoptionListContainer extends StatelessWidget {
-  final AdoptionList details;
-  const AdoptionListContainer({super.key, required this.details});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        // Container(
-        //   height: 180,
-        //   width: 160,
-        //   decoration: BoxDecoration(
-        //     borderRadius: BorderRadius.circular(20),
-        //     color: kParimaryColor,
-        //   ),
-        //   child: Center(
-        //     child: Image.asset(
-        //       details.image,
-        //       // fit: BoxFit.fill,
-        //     ),
-        //   ),
-        // ),
-        AspectRatio(
-          aspectRatio: 1.0,
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20), color: kParimaryColor),
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset(
-                  details.image,
-                  fit: BoxFit.scaleDown,
-                )),
-          ),
-        ),
-        Text(
-          details.title,
-          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 15),
-        ),
-        Text(
-          'Age: ' + details.age.toString(),
-          style: TextStyle(fontWeight: FontWeight.bold, color: kParimaryColor),
-        )
-      ],
     );
   }
 }
